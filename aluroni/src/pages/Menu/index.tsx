8a6120d83bf1ef35bc92fe737958ec14bf9a8aaa
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-05-25 21:21:58 -0300

    Created header, scss variables and BEM classes used

--- aluroni/src/pages/Menu/index.tsx
+++ aluroni/src/pages/Menu/index.tsx
@@ -6,8 +6,11 @@ export default function Menu(){
         <main>
             <nav className={styles.menu}>
                 <Logo/>
-                Aluroni
             </nav>
+
+            <header className={styles.header}>
+                <div className={styles.header__text}>A casa do código e da massa</div>
+            </header>
         </main>
     )
 }

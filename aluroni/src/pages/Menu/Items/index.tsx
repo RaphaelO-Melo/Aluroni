240f0ab2b53c23c912bf14ddf3188c1116e8ab5b
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-05-31 20:41:18 -0300

    Functional filter

--- aluroni/src/pages/Menu/Items/index.tsx
+++ aluroni/src/pages/Menu/Items/index.tsx
@@ -1,11 +1,46 @@
 import items from './items.json';
 import Item from './Item';
 import style from './Items.module.scss';
+import { useEffect, useState } from 'react';
 
-export default function Items(){
+interface Props {
+    search: string,
+    filter: number | null,
+    sorter: string
+}
+
+export default function Items(props : Props){
+
+    const [list, setList] = useState(items);
+    const {search, filter } = props;
+
+    
+    useEffect(() => {
+
+        const newList = items.filter(item => testSearch(item.title) && testFilter(item.category.id));
+        setList(newList);
+
+    }, [search, filter]);
+    
+
+    function testSearch(title: string){
+        const regex = new RegExp(search, 'i');
+        return regex.test(title);
+    }
+
+    function testFilter(id: number){
+        
+        if(filter !== null){
+            return filter === id;
+        } else {
+            return true;
+        }
+
+    }
+
     return (
         <div>
-            {items.map(item => (
+            {list.map(item => (
                 <div className={style.items}>
                     <Item key={item.id} {...item}/>
                 </div>

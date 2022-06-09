06a08b96d96d4ec2450dc913f709098e7f426ad7
Raphael O. Melo <raphael.o.melo@outlook.com>
2022-06-09 20:07:16 -0300

    Created first route using Router

--- aluroni/src/index.tsx
+++ aluroni/src/index.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-import Menu from './pages/Menu';
-import Start from 'pages/Start';
+import Router from './routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <Start />
+        <Router />
     </React.StrictMode>
 );

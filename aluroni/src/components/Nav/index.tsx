c3254d54145171663a07d08a30f6a5b5b850b866
Raphael Oliveira Melo <raphael.o.melo@outlook.com>
2022-06-10 18:58:24 -0300

    Replaced a with link component

--- aluroni/src/components/Nav/index.tsx
+++ aluroni/src/components/Nav/index.tsx
@@ -1,5 +1,6 @@
 import {ReactComponent as Logo} from 'assets/logo.svg';
 import styles from './Nav.module.scss';
+import { Link } from 'react-router-dom';
 
 export default function Nav(){
 
@@ -15,7 +16,9 @@ export default function Nav(){
             <ul className={styles.menu__list}>
                 {routes.map((route, index) => (
                     <li key={index} className={styles.menu__link}>
-                        <a href={route.to}>{route.label}</a>
+                        <Link to={route.to}>
+                            {route.label}
+                        </Link>
                     </li>
                 ))}
             </ul>

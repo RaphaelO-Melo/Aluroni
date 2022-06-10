07d68889021e0fa74b5864da6f0a3b241ef03c57
Raphael Oliveira Melo <raphael.o.melo@outlook.com>
2022-06-10 18:50:05 -0300

    Nav menu with links

--- aluroni/src/components/Nav/index.tsx
+++ aluroni/src/components/Nav/index.tsx
@@ -2,9 +2,23 @@ import {ReactComponent as Logo} from 'assets/logo.svg';
 import styles from './Nav.module.scss';
 
 export default function Nav(){
+
+    const routes = [
+        {label: 'Início', to: '/'},
+        {label: 'Menu', to: '/menu'},
+        {label: 'Sobre', to: '/sobre'},
+    ];
+
     return (
         <nav className={styles.menu}>
             <Logo/>
+            <ul className={styles.menu__list}>
+                {routes.map((route, index) => (
+                    <li key={index} className={styles.menu__link}>
+                        <a href={route.to}>{route.label}</a>
+                    </li>
+                ))}
+            </ul>
         </nav>
     );
 }
